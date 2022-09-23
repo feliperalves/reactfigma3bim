9854abc1f6d1f7f36623be906dca49d90ab657c9
feliperalves <feliperchalves@gmail.com>
2022-09-23 10:37:12 -0300

    Aula do dia 23/09/2022

--- src/services/data/User/index.ts
+++ src/services/data/User/index.ts
@@ -1,14 +1,14 @@
-
 import { IResponseUser, IUser } from "interfaces/user.interface";
-import api from "services/api";
+import api from "services/api"
+import User from "./User";
 
 class UserData {
-  register(data: IUser) {
-    return api.post<IResponseUser>('/register', data)
-  }
-  login(data: IUser) {
-    return api.post<IResponseUser>('/login', data)
-  }
+    register(data: IUser) {
+        return api.post<IResponseUser>('/register', data)
+    }
+    login(data: IUser) {
+        return api.post<IResponseUser>('/login', data)
+    }
 }
 
 export default new UserData()
