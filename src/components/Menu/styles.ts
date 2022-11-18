200a31c4c417ac69da52b364d225fe1a0bf142bb
feliperalves <feliperchalves@gmail.com>
2022-11-17 22:17:41 -0300

    feito

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,12 +1,11 @@
 import styled from "styled-components"
 import { colors } from "styles/GlobalStyle"
-
 export const Cabecalho = styled.header`
   display: flex;
   border-bottom: 0.5rem solid ${colors.secondary};
-  h1 {
-    color: #FF0000;
-    text-decoration: none;
+  picture svg {
+    height: 6rem;
+    width: 6rem;
   }
   nav {
     width: 100%;
@@ -23,6 +22,17 @@ export const Cabecalho = styled.header`
           text-decoration: none;
           font-size: 1.2em;
           color: ${colors.primary};
+        }
+        button {
+          display: flex;
+          gap: 0.5rem;
+          align-items: center;
+          text-decoration: none;
+          font-size: 1.2em;
+          color: ${colors.primary};
+          border: 0;
+          background-color: transparent;
+          cursor: pointer;
         }
       }
     }
